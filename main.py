50d89569c48223d61fd668695ed4652b48fbc3f1
nosiychuk_daniil <nosiychuk_daniil@yahoo.com>
2022-11-06 21:23:44 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Restaurant():
+    """Простая модель ресторана"""
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name=restaurant_name
+        self.cuisine_type=cuisine_type
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} - {self.cuisine_type}")
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open!")
+
+my_rest = Restaurant("MD","trash")
+my_rest.open_restaurant()
+my_rest.describe_restaurant()
